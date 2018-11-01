1843017ae941b4db7ee1a4501c637f1116399503
John SUmskas <john.sumskas@gmail.com>
2018-11-01 17:41:50 +1100

    Bug fixes/improvements for lab motor tests

--- nodes/lab_test_motors.py
+++ nodes/lab_test_motors.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     rospy.init_node('labtestmotors', anonymous=True)
 
     command_limit = 200.
-    time_period_limit = 20.
+    time_period_limit = 10.
 
     lpub = rospy.Publisher("/left_thrust_cmd",Float32,queue_size=1)
     rpub = rospy.Publisher("/right_thrust_cmd",Float32,queue_size=1)
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     thrust_on = True
     publish_thrust = True
     time_period = random.uniform(0, time_period_limit)
-    time_last = rospy.get_rostime()
+    time_last = rospy.get_rostime().to_sec()
 
     rate = rospy.Rate(10)
 
@@ -35,11 +35,20 @@ if __name__ == '__main__':
                     thrust_msg.data = thrust_cmd
                     lpub.publish(thrust_cmd)
                     rpub.publish(thrust_cmd)
+                    print("Left %f, Right %f" %(thrust_cmd, thrust_cmd))
+                else:
+                    thrust_msg = Float32()
+                    thrust_msg.data = 0.0
+                    lpub.publish(thrust_msg)
+                    rpub.publish(thrust_msg)
+                    print("Left %f, Right %f" %(0.0, 0.0))
+            else:
+                print("Not publishing")
             rate.sleep()
-            if rospy.get_rostime() > time_last + time_period:
-                time_last = rospy.get_rostime()
+            if rospy.get_rostime().to_sec() > time_last + time_period:
+                time_last = rospy.get_rostime().to_sec()
                 time_period = random.uniform(0, time_period_limit)
-                thrust_on = bool(random.randint(0, 1))
-                publish_thrust = bool(random.randint(0, 1))
+                thrust_on = bool(random.randint(0, 2))
+                publish_thrust = bool(random.randint(0, 2))
     except rospy.ROSInterruptException:
         pass
