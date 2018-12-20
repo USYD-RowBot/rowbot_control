8b92e04dea90a4f7f876f03e1b853a194eafd9e4
Jackson Shields <jacksonhshields@gmail.com>
2018-12-19 21:38:38 -1000

    Added path to waypoints - which translates a nav_msgs::Path to rowbot_msgs::VesselPath

---
+++ src/path_to_waypoints.cpp
@@ -0,0 +1,67 @@
+/*
+ * Path To Waypoints
+ * Purpose: Receives a nav_msgs/Path and makes it a nav_msgs/VesselPath
+ * Inputs:
+ *  - Path: nav_msgs/Path
+ * Outputs:
+ *  - Waypoints: rowbot_msgs/VesselPath
+*/
+#include <math.h>
+#include <vector>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "ros/ros.h"
+#include "nav_msgs/Odometry.h"
+#include "tf/transform_datatypes.h"
+#include "rowbot_msgs/VesselWaypoint.h"
+#include "rowbot_msgs/VesselPath.h"
+
+class PathToWaypoints
+{
+public:
+    PathToWaypoints()
+	{
+            // ros::NodeHandle n_("~");
+            waypointPub_ = n_.advertise<rowbot_msgs::VesselPath>("/waypoints", 1)
+            pathSub_ =  n_.subscribe("/path", 1, &PathToWaypoints::pathCallback, this);
+            ros::param::get("~tolerance", tolerance_)
+    }
+    
+    //Path Callback
+    void pathCallback(const nav_msgs::Path::ConstPtr& msg)
+    {
+        rowbot_msgs::VesselPath wpPath;
+        for(n=0;n<msg->poses.size(), n++)
+        {
+            rowbot_msgs::VesselWaypoint wp;
+            wp.pose = msg->poses.at(n);
+            wp.tolerance = tolerance_;
+        }
+        waypointPub_.publish(wpPath);
+
+    } 
+
+private:
+	ros::NodeHandle n_;
+    ros::Subscriber pathSub_;
+    ros::Publisher waypointPub_;
+
+    double tolerance_;
+
+};
+
+
+int main(int argc, char **argv)
+{
+
+  ros::init(argc, argv, "path_to_waypoint");
+
+  PathToWaypoint p2w;
+
+  ros::spin();
+
+
+  return 0;
+}
